c10564b25bda75cf5fd62831c57f54e818cb04ac
Parker Bossier <parkerbossier@gmail.com>
2025-08-14 08:06:37 -0400

    Vite

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,15 @@
 /// <reference types="react-scripts" />
 
+declare module "*.css";
+declare module "*.module.css";
+declare module "*.module.scss";
+
+declare module "*.jpg";
+declare module "*.png";
+
 declare namespace Wordle {
 	type GameState = Wordle.TileState[][];
-	
+
 	type TileStatus = 'absent' | 'correct' | 'none' | 'present';
 
 	interface TileState {
